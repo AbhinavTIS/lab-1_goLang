625df9a9f60bfd6b766ffa5b3a15e354b7590299
Abhinav Mahajan <96789490+AbhinavTIS@users.noreply.github.com>
2024-01-28 18:07:32 -0500

    add isLeap.go

---
+++ isLeap.go
@@ -0,0 +1,15 @@
+package main
+
+func isLeapYear(year int) bool {
+
+	if year%4 == 0 {
+
+		if year%100 != 0 || year%100 == 0 && year%400 == 0 {
+
+			return true //
+		}
+
+	}
+
+	return false // compiler will reach this stage only if the number is non-leap
+}
